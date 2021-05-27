0e350735762364630a9b0a53ef7e71162949f89b
Amneh Alsuqi <aalsu013@ucr.edu>
2021-05-27 10:38:17 -0700

    personal task class

---
+++ personal_task.hpp
@@ -0,0 +1,17 @@
+#ifndef __PERSONAL_TASK_HPP
+#define __PERSONAL_TASK_HPP
+
+
+#include "task.hpp"
+
+class PersonalTask : public Task {
+
+
+	public: 
+		void Add_PersonalTask();
+		//SchoolTask();
+
+};
+
+
+#endif
