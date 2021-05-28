5eb97cb0c52973c9352ce8ce5e991c54cb1f6110
alsuqiamneh <81607334+alsuqiamneh@users.noreply.github.com>
2021-05-27 18:18:57 -0700

    Update task.cpp

--- task.cpp
+++ task.cpp
@@ -70,43 +70,6 @@ int Task::getPriority(){
 
 
  
-void Task::DisplayMenu(){
-	string input;
-	
-	cout << "Welcome to PlannerPlus. Enter the corresponding number for the following options follwed by [ENTER]." << endl;
-		
-	cout << "1. Create New Task." << endl;
-	cout << "2. Display Existing Task(s)." << endl;
-	cout << "3. Delete a Task." << endl;
-	cout << "4. Edit an Existing Task" << endl;
-	cout << "5. Quit." << endl;
-
-	cin >> input;
-	
-	if (input == "1") {
-		cout << "What type of task would you like to create? Enter the corresponding number for the following options follwed by [ENTER]." << endl;
-		cout << "1. School Task." << endl;
-		cout << "2. Personal Task." << endl;
-		cout << "3. Go back to the main menu." << endl;
-		cin >> input;
-		if (input == "1"){
-			cout << "You are creating a school task." << endl;
- 		//	task_description();
- 		cout << "Create a title for your task follwed by [ENTER]." << endl;
-		cin.ignore();		
-		getline(cin, task_name);
-		cout << "You entered: " << getName() << endl;
-		
-		}
-	}
-
-	else if (input == "2"){
-	}
-	else if (input == "3"){
-	}
-	else if (input == "4") {
-	}
-			
-}
+//void Task::DisplayMenu(){}
 
 
