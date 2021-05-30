79f4c399e80f4751e7c0c13fd05845f9d6a8ee66
PatrickPicca <patrickpicca@gmail.com>
2021-05-30 14:32:44 -0700

    Current

--- SortCategory.cpp
+++ SortCategory.cpp
@@ -11,17 +11,38 @@ class SortCategory : public SortClass
 
 	public:
 		virtual void Filter(vector<Task> &vect){
-		
+	
+			std::cout << "How do you wish to sort your tasks by?" << endl;
+			std::cout << "1. School tasks" << endl;
+			std::cout << "2. Personal tasks" << endl;
+				
 			string category;
+
+			std::cin >> category;
 			vector<Task> tempList;
 
+			if (category == "1")
+			{
+				for (int i = 0; i < vect.size(); i++){
+					if 
+				}
+			}
+			else if (category == "2")
+			{
+
+			}
+			else{
+				std::cout << "Invalid input!" << endl;
+				Filter(vect);
+			}
+
 			for (int i = 0; i < vect.size(); i++){
-				if (vect[i].getCategory == category)
+				if (vect[i].getCategory() == category)
 					tempList.push_back(vect[i]);
 			}			
 
 			for (int i = 0; i < vect.size(); i++){
-				if (vect[i].getCategory != category)
+				if (vect[i].getCategory() != category)
 					tempList.push_back(vect[i]);
 			}
 						
@@ -35,7 +56,7 @@ class SortCategory : public SortClass
 			vector<Task> tempList;
 
 			for (int i = 0; i < vect.size(); i++){
-				if (vect[i].getCategory == category)
+				if (vect[i].getCategory() == category)
 					tempList.push_back(vect[i]);
 			}			
 						
