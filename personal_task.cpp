5dd482f435b7b973507a44e6e363c0edd17cd0cd
Amneh Alsuqi <aalsu013@ucr.edu>
2021-05-30 04:32:33 -0700

    updated + working

--- personal_task.cpp
+++ personal_task.cpp
@@ -26,20 +26,21 @@ PersonalTask::PersonalTask(string category, string name, string description){
 		getline(cin, task_description);
 		cout << "You entered: " << getDescription() << endl;
 
-		cout << "Set a date you'd like to complete " << getName() << "by. Answer the following and hit [ENTER] to save." << endl;
-		cout << "What day would you like to finish " << getName() << " by? Enter a valid day of the month." << endl;
+		cout << "Set a date you'd like to complete " << getName() << " by. Answer the following and hit [ENTER] to save." << endl;
+		cout << endl;
+		cout << "What day of the month would you like to finish " << getName() << " by? Enter a valid day as an integer." << endl;
 		
 		//cin.ignore();
 		getline(cin, temp);
 		day = std::stoi(temp);
 		while (day < 1 || day > 31){
-			cout << "Invalid input. Please enter a valid day of the month followed by [ENTER]." << endl;
+			cout << "Invalid input. Please enter a valid day of the month as an integer followed by [ENTER]." << endl;
 			//cin.ignore();
 			getline(cin, temp);
 			day = stoi(temp);
 		}
 		cout << "You entered: " << getDay() << endl;
-		
+		cout << endl;
 		cout << "What month do you plan on finishing " << getName() << "? Enter a valid month as an integer." << endl;
 		
 		//cin.ignore();
@@ -52,8 +53,8 @@ PersonalTask::PersonalTask(string category, string name, string description){
 			month = stoi(temp);
 		}
 		cout << "You entered: " << getMonth() << endl;
-		
-		cout << "What year would you like to finish " << getName() << "by? Enter a valid year as an integer." << endl;
+		cout << endl;
+		cout << "What year would you like to finish " << getName() << " by? Enter a valid year as an integer." << endl;
 		
 		//cin.ignore();
                 getline(cin, temp);
@@ -65,15 +66,95 @@ PersonalTask::PersonalTask(string category, string name, string description){
 			year = stoi(temp);
 		}
 		cout << "You entered: " << getYear() << endl;
-
-		cout <<  "You wish to complete " << getName() << " by: " << getDay() << "/" << getMonth() << "/" << getYear() << endl;
-	
+		cout << endl;
+		cout <<  "You wish to complete " << getName() << " by: " << getMonth() << "/" << getDay() << "/" << getYear() << endl;
+		cout << endl;
 		cout << getName() << " is now saved in your Planner." << endl;
-
+		cout << endl;
 
 	}
 
 	PersonalTask::PersonalTask(){}
 
 
-	void PersonalTask::EditTask() {}
+	void PersonalTask::EditTask() {
+		string edit_input;
+                string temp;
+	
+                cout << "Which of the following would you like to edit? Select the corresponding number followed by [ENTER]." << endl;
+                cout << "1. Task name" << endl;
+                cout << "2. Task description" << endl;
+                cout << "3. Task date" << endl;
+	
+                cin.ignore();
+		cin >> edit_input;
+		while(edit_input != "1" && edit_input!= "2" && edit_input!= "3"){
+                                cout << "Invalid input. Please try again." << endl;
+				//cin.ignore();
+                		cin >> edit_input;
+		}
+		if(edit_input == "1") {
+			task_name = "";
+			cout << "Type your new task name followed by [ENTER]." << endl;
+			//cin.ignore();
+			getline(cin, task_name);
+                	
+			cout << "You entered: " << getName() << endl; 	
+			cout << endl;
+			cout << "Your task name has been changed." << endl;
+		}
+		else if(edit_input == "2") {
+			task_description = "";
+			cout << "Type your new task description for " << getName() << " followed by [ENTER]." << endl;
+			//cin.ignore();
+			getline(cin, task_description);
+
+			cout << "You entered: " << getDescription() << endl;
+			cout << endl;
+			cout << "Your task description has been changed." << endl;
+		}
+		else {
+			day = 0;
+			month = 0;
+			year = 0;
+			cout << "You are changing the date you'd like to complete " << getName() << " by. Please answer the following questions with valid inputs." << endl;
+			cout << endl;
+			cout << "What day of the month would you like to complete " << getName() << "? Enter a valid day as an integer." << endl;
+
+         		        //cin.ignore();
+               		        getline(cin, temp);
+               			day = std::stoi(temp);
+                		while (day < 1 || day > 31){
+                        		cout << "Invalid input. Please enter a valid day of the month followed by [ENTER]." << endl;
+                        		cin.ignore();
+                        	//	getline(cin, temp);
+                        		day = stoi(temp);
+                		}
+
+		                cout << "What month would you like to complete " << getName() << "? Enter a valid month as an integer." << endl;
+
+                		//cin.ignore();
+                		getline(cin, temp);
+                		month = std::stoi(temp);
+                		while(month < 1 || month > 12) {
+                        		cout << "Invalid input. Please enter a valid month of the year as an integer followed by [ENTER]." << endl;
+                        	//	cin.ignore();
+                        		getline(cin, temp);
+                        		month = stoi(temp);
+                		}		
+
+	                cout << "What year would you like to complete " << getName() << "? Enter a valid year as an integer." << endl;
+
+        	       // cin.ignore();
+                	getline(cin, temp);
+                	year = stoi(temp);
+                	while (year < 2021 || year > 3000){
+                        	cout << "Invalid input. Please enter a valid year as an integer followed by [ENTER]." << endl;
+                        //	cin.ignore();
+                        	getline(cin, temp);
+                        	year = stoi(temp);
+       		       }
+			cout << "You have saved the following date to complete " << getName() << ": " << getMonth() << "/" << getDay() << "/" << getYear() << endl;
+	
+		}	
+}	
