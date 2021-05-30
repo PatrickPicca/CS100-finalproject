0904cd05428eabe93cc126d3f0437bbd46c1a4bc
Amneh Alsuqi <aalsu013@ucr.edu>
2021-05-29 21:30:28 -0700

    fixes

--- personal_task.cpp
+++ personal_task.cpp
@@ -21,19 +21,19 @@ PersonalTask::PersonalTask(string category, string name, string description){
 		cout << "You entered: " << getName() << endl;
 		
 		cout << "Create a description for " <<  getName() << ". Once completed, hit [ENTER] to save." << endl;
-		cin.ignore();
+		//cin.ignore();
 		getline(cin, task_description);
 		cout << "You entered: " << getDescription() << endl;
 
 		cout << "Set a date you'd like to complete " << getName() << "by. Answer the following and hit [ENTER] to save." << endl;
 		cout << "What day would you like to finish " << getName() << " by? Enter a valid day of the month." << endl;
 		
-		cin.ignore();
+		//cin.ignore();
 		getline(cin, temp);
 		day = std::stoi(temp);
 		while (day < 1 || day > 31){
 			cout << "Invalid input. Please enter a valid day of the month followed by [ENTER]." << endl;
-			cin.ignore();
+			//cin.ignore();
 			getline(cin, temp);
 			day = stoi(temp);
 		}
@@ -41,12 +41,12 @@ PersonalTask::PersonalTask(string category, string name, string description){
 		
 		cout << "What month do you plan on finishing " << getName() << "? Enter a valid month as an integer." << endl;
 		
-		cin.ignore();
+		//cin.ignore();
 		getline(cin, temp);
 		month = std::stoi(temp);
 		while(month < 1 || month > 12) {
 			cout << "Invalid input. Please enter a valid month of the year as an integer followed by [ENTER]." << endl;
-			cin.ignore();
+			//cin.ignore();
 			getline(cin, temp);
 			month = stoi(temp);
 		}
@@ -54,12 +54,12 @@ PersonalTask::PersonalTask(string category, string name, string description){
 		
 		cout << "What year would you like to finish " << getName() << "by? Enter a valid year as an integer." << endl;
 		
-		cin.ignore();
+		//cin.ignore();
                 getline(cin, temp);
 		year = stoi(temp);
 		while (year < 2021 || year > 3000){
 			cout << "Invalid input. Please enter a valid year as an integer followed by [ENTER]." << endl;
-                        cin.ignore();
+                 //       cin.ignore();
                         getline(cin, temp);
 			year = stoi(temp);
 		}
